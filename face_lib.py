b3f1023dd959f22ba44ec73940ccb410867a7cd2
a-akram-98 <a.akram.9821@gmail.com>
2021-10-11 15:55:52 +0200

    adding face_embeddings method

--- face_lib.py
+++ face_lib.py
@@ -6,6 +6,7 @@ class face_lib:
         
         self.frontalClassfier = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
         self.profileClassfier = cv2.CascadeClassifier("haarcascade_profileface.xml")
+        self.faceEmbeddingNet = cv2.dnn.readNetFromTorch("nn4.small2.v1.t7")
     
 
     def face_detection(self, frame):
@@ -13,7 +14,7 @@ class face_lib:
             input: frame given by cv.VideoCapture
 
             return: [int] no. of faces detected in images, 
-                    [list] list of faces in shape of (x, y, w, h)
+                    [list] list of faces coordinates in shape of (x, y, w, h)
         
         """
 
@@ -27,8 +28,42 @@ class face_lib:
         
         no_faces = len(faces)
 
-        return no_faces, faces
+        return no_faces, faces_coors   
+
+    def face_recognition(self, face_coors, gt, frame):
+        """
+        input: face to verify [output from face_detection],
+                ground_truth image
+        output: verfied (boolen), True verified, False unverified
+
+        """
+        ######### preproces face ##########
+        (x, y, w, h) = face_coors
+        face = frame[x:x+h,y:y+h]
+        face = cv2.resize(face, (96,96))
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+        ###################################
+
+        ######### preprocess GT ###########
+        gt = cv2.cvtColor(gt, cv2.COLOR_BGR2RGB)
+        gt = cv2.resize(gt, (96,96))
+        ###################################
+
+        ####### get faceEmbeddings #######
+        face_embeddings = self.face_embeddings(face)
+        gt_embeddings = self.face_embeddings(gt)
+        ######################################
     
 
-    def face_recognition(self):
-        raise NotImplementedError(self.__class__.__name__ + '.face_recognition')
+    def face_embeddings(self, face):
+        """
+        input: image of face with dimensions of 96x96
+        return: face embeddings of the image
+        """
+        ###### get blobs from images ######
+        face_blob = cv2.dnn.blobFromImage(face)
+        ###################################
+
+        self.faceEmbeddingNet.setInput(face_blob)
+        faceEmbeddings = self.faceEmbeddingNet.forward()
+        return faceEmbeddings
